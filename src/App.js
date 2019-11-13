0cfa418a3128c52068ea5d1bc1559879fe1f3993
Mark Woodward <mwoodward23@hotmail.co.uk>
2019-11-13 10:15:23 +0000

    wip

--- src/App.js
+++ src/App.js
@@ -1,23 +1,71 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+
+const Block = () => (
+  <div style={{ width: '25px', height: '25px', backgroundColor: 'red' }} />
+)
+
+const length1 = [0];
+const length2 = [0, 1];
+const length3 = [0, 1, 2];
+const length4 = [0, 1, 2, 3];
+const length5 = [0, 1, 2, 3, 4];
+const length6 = [0, 1, 2, 3, 4, 5];
+const length7 = [0, 1, 2, 3, 4, 5, 6];
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="grid" style={{ paddingTop: '50px'}}>
+          {length1.map((_, i) => (
+          <div className={`grid-item-${i}`}>
+            <Block />
+          </div>
+        ))}
+        </div>
+        <div className="grid" style={{ paddingTop: '50px'}}>
+          {length2.map((_, i) => (
+          <div className={`grid-item-${i}`}>
+            <Block />
+          </div>
+        ))}
+        </div>
+        <div className="grid" style={{ paddingTop: '50px'}}>
+          {length3.map((_, i) => (
+          <div className={`grid-item-${i}`}>
+            <Block />
+          </div>
+        ))}
+        </div>
+        <div className="grid" style={{ paddingTop: '50px'}}>
+          {length4.map((_, i) => (
+          <div className={`grid-item-${i}`}>
+            <Block />
+          </div>
+        ))}
+        </div>
+        <div className="grid" style={{ paddingTop: '50px'}}>
+          {length5.map((_, i) => (
+          <div className={`grid-item-${i}`}>
+            <Block />
+          </div>
+        ))}
+        </div>
+        <div className="grid" style={{ paddingTop: '50px'}}>
+          {length6.map((_, i) => (
+          <div className={`grid-item-${i}`}>
+            <Block />
+          </div>
+        ))}
+        </div>
+        <div className="grid" style={{ paddingTop: '50px'}}>
+          {length7.map((_, i) => (
+          <div className={`grid-item-${i}`}>
+            <Block />
+          </div>
+        ))}
+        </div>
       </header>
     </div>
   );
